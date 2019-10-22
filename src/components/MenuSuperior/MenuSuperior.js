124edc18bd362c728d3dbf3ea9372f39dd2a013e
matheussantos-dev <matheussm301@gmail.com>
2019-10-21 23:56:10 -0300

    add mudle popper jquery bootstrap

---
+++ src/components/MenuSuperior/MenuSuperior.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class MenuSuperior extends Component {
+  render() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            
+        </nav>
+    )
+  }
+}
