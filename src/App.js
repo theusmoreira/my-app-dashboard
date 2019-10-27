3fa947b8b75cebb15cba38c1fe94b0e359a365fc
Matheus Santos <matheussm301@gmail.com>
2019-10-27 03:23:32 -0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import { Switch, Route } from 'react-router-dom';
+
 import MenuSuperior from './components/MenuSuperior/MenuSuperior';
 import Resumo from './components/Resumo/Resumo';
 import Consulta from './components/Consultas/Consulta';
@@ -10,9 +12,17 @@ class App extends Component {
     return (
       <div>
         <MenuSuperior />
-        <Resumo />
-        <Consulta />
-        <Faturamento />
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col">
+              <Switch>
+                <Route path="/" exact component={Resumo} />
+                <Route path="/consultas" component={Consulta} />
+                <Route path="/faturamento" component={Faturamento} />
+              </Switch>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
