c34f18cd07fab9a8510c2fbe96e9382a22bce7e2
Matheus Santos <matheussm301@gmail.com>
2019-10-27 01:36:07 -0300

    add new components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 
 import MenuSuperior from './components/MenuSuperior/MenuSuperior';
+import Resumo from './components/Resumo/Resumo';
+import Consulta from './components/Consultas/Consulta';
+import Faturamento from './components/Faturamento/Faturamento';
 
 class App extends Component {
   render() {
     return (
       <div>
         <MenuSuperior />
-
-      
+        <Resumo />
+        <Consulta />
+        <Faturamento />
       </div>
     )
   }
